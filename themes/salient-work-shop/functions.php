d2d1c700eaee61893ab88cbebd735905dca29760
Nic Schumann <nicolas_schumann@brown.edu>
2015-08-27 13:39:04 -0400

    renamed the Portfolio PT to Press

--- themes/salient-work-shop/functions.php
+++ themes/salient-work-shop/functions.php
@@ -39,4 +39,33 @@ function add_menu_posttype() {
 }
 add_action('init', 'add_menu_posttype');
 
+/**
+ * rename_portfolio_post_type : void -> void
+ * This method renames the portfolio post type on child-theme load
+ *
+ * @hooked init
+ */
+function rename_portfolio_post_type() {
+	global $wp_post_types;
+	$p = "portfolio";
+
+	if ( empty( $wp_post_types[ $p ] ) || ! is_object( $wp_post_types[ $p ] ) || empty( $wp_post_types[ $p ]->labels ) ) { return; }
+
+	$wp_post_types[ $p ]->labels->name = "Press";
+	$wp_post_types[ $p ]->labels->singular_name = "Press";
+	$wp_post_types[ $p ]->labels->add_new = "Add Press Item";
+	$wp_post_types[ $p ]->labels->add_new_item = "Add New Press Item";
+	$wp_post_types[ $p ]->labels->all_items = "All Press";
+	$wp_post_types[ $p ]->labels->edit_item = "Edit Press";
+	$wp_post_types[ $p ]->labels->name_admin_bar = "Press";
+	$wp_post_types[ $p ]->labels->menu_name = "Press";
+	$wp_post_types[ $p ]->labels->new_item = "New Press Item";
+	$wp_post_types[ $p ]->labels->not_found = "New Press Items found";
+	$wp_post_types[ $p ]->labels->not_found_in_trash = "New Press Items found in Trash";
+	$wp_post_types[ $p ]->labels->search_items = "Search Press";
+	$wp_post_types[ $p ]->labels->view_item = "View Press Item";
+
+}
+add_action('wp_loaded', 'rename_portfolio_post_type');
+
 ?>
